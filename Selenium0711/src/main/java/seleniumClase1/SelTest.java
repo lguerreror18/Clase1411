833bef24cf04b7562d352a3b797a4e5afcacb216
Inteligo <Inteligo@Alquiler-12>
2020-11-14 10:10:32 -0500

    First commit

---
+++ Selenium0711/src/main/java/seleniumClase1/SelTest.java
@@ -0,0 +1,29 @@
+package seleniumClase1;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SelTest {
+
+	public static void main(String[] args) {
+
+			System.out.println("Test");
+			
+			// Seteando el Path de nuestro driver, donde esta guardado
+			String exePath = Paths.get("").toAbsolutePath().toString() + File.separator + "drivers" + File.separator;
+			
+			// Identificando el .exe para driver
+			System.setProperty("webdriver.chrome.driver", exePath + "chromedriver.exe");
+			
+			// Creación en tiempo de ejecucion
+			WebDriver driver = new ChromeDriver();
+			
+			// Abriendo con la URL especificada
+			driver.get("https://www.cooltesters.com/tienda-de-cursos/selenium-con-java");
+			
+	}
+
+}
---
+++ Selenium0711/src/test/java/Test/TestNGEj.java
@@ -0,0 +1,11 @@
+package Test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestNGEj {
+  @Test
+  public void f() {
+	  Assert.assertTrue(true);
+  }
+}
